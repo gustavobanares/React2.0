1b9b185178a00cce521e0d58a097287220188518
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-09-19 00:37:44 -0300

    Use images in react

--- learning/src/App.jsx
+++ learning/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import City from './assets/city.jpg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Avancando em React</h1>
+      {/* Imagem em public */}
+      <div>
+        <img src="/img1.jpg" alt="Paisagem" />
+      </div>
+      {/* Imagem em assets */}
+      <div>
+      <img src={City} alt="Cidade" />
+      </div>
     </>
   )
 }
