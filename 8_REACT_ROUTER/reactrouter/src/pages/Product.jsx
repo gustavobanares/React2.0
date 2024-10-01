d9871045b76922e8216351ad7d04e1df62c01095
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-10-01 20:45:55 -0300

    Dynamic data loading

--- 8_REACT_ROUTER/reactrouter/src/pages/Product.jsx
+++ 8_REACT_ROUTER/reactrouter/src/pages/Product.jsx
@@ -1,15 +1,30 @@
-import { useParams } from "react-router-dom"
+import { useFetch } from "../hooks/useFetch";
 
+import { useParams, Link } from "react-router-dom";
 
 const Product = () => {
-const {id} = useParams()
+  //4  - rota dinamica
+  const { id } = useParams();
 
+  //5 - carregamento de dados
+  const url = "http://localhost:3000/products/" + id;
+
+  const { data: product, loading, error } = useFetch(url);
 
   return (
     <>
-    <p>ID do produto: {id}</p>
-    </>
-  )
-}
+      <p>ID do produto: {id}</p>
 
-export default Product
+      {error && <p>Ocorreu um erro...</p>}
+      {loading && <p>Carregando produto...</p>}
+      {product &&(
+        <div>
+            <h1>{product.name}</h1>
+            <p>R${product.price}</p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Product;
