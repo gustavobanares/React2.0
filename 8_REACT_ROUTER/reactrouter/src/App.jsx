5c9e59fa6dcff098090dc0f9a58dcb9d7b617683
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-10-01 20:06:11 -0300

    Navigation between pages

--- 8_REACT_ROUTER/reactrouter/src/App.jsx
+++ 8_REACT_ROUTER/reactrouter/src/App.jsx
@@ -7,12 +7,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import About from '../pages/About'
 
+// components
+import Navbar from '../components/Navbar'
+
 function App() {
 
   return (
     <div>
       <h1>React Router</h1>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
