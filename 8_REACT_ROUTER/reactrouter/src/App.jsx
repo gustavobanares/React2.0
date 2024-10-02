054746fad9734b1f2562e8346f70f6ae5d486a51
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-10-02 18:47:35 -0300

    Page 404

--- 8_REACT_ROUTER/reactrouter/src/App.jsx
+++ 8_REACT_ROUTER/reactrouter/src/App.jsx
@@ -11,6 +11,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Product from './pages/Product'
 import Info from './pages/Info'
+import NotFound from './pages/NotFound'
 
 function App() {
 
@@ -27,7 +28,8 @@ function App() {
           <Route path='/products/:id/info' element={<Info />} />
           {/* 4 - rota dinamica */}
           <Route path='/products/:id' element={<Product />} />
-          
+          {/* 7 - nop match route */}
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
