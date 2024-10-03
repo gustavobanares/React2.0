a24859b5170133f042fbc1c8484be9d69fe9114e
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-10-02 21:34:01 -0300

    Changing context

--- 9_CONTEXT_API/contextapi/src/pages/Home.jsx
+++ 9_CONTEXT_API/contextapi/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 
 import {CounterContext} from "../context/CounterContext" 
+import ChangeCounter from "../components/ChangeCounter"
 
 
 const Home = () => {
@@ -11,6 +12,8 @@ const Home = () => {
     <div>
         <h1>Home</h1>
         <p>Valor do contador: {counter}</p>
+        {/* 3 - alterando valor contexto */}
+        <ChangeCounter />
     </div>
   )
 }
--- 9_CONTEXT_API/contextapi/src/pages/Other.jsx
+++ 9_CONTEXT_API/contextapi/src/pages/Other.jsx
@@ -1,7 +1,17 @@
+import { useContext } from "react"
+
+import { CounterContext } from "../context/CounterContext"
+
 
 const Other = () => {
+
+  const {counter} = useContext(CounterContext)
+
   return (
-    <div>Other</div>
+    <div>
+      <h1>Other</h1>
+      <p>Valor do contador: {counter}</p>
+    </div>
   )
 }
 
