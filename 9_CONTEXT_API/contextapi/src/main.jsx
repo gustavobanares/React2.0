484df274334842f42749063a5e644d6d9a9e54f8
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-10-03 23:53:58 -0300

    Creating a more complex context

--- 9_CONTEXT_API/contextapi/src/main.jsx
+++ 9_CONTEXT_API/contextapi/src/main.jsx
@@ -4,12 +4,15 @@ import App from './App.jsx'
 import './index.css'
 
 import { CounterContextProvider } from './context/CounterContext.jsx'
+import { TitleColorContextProvider } from './context/TitleColorContext.jsx' 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* 2 - criando provider */}
     <CounterContextProvider>
-      <App />
+      <TitleColorContextProvider>
+        <App />
+      </TitleColorContextProvider>
     </CounterContextProvider>
   </StrictMode>,
 )
--- 9_CONTEXT_API/contextapi/src/pages/Home.jsx
+++ 9_CONTEXT_API/contextapi/src/pages/Home.jsx
@@ -6,15 +6,23 @@ import ChangeCounter from "../components/ChangeCounter"
 // 4 - refatorando um hook
 import { useCounterContext } from "../hooks/useCounterContext"
 
+// 5- context mais complexo
+import { useTitleColorContext } from "../hooks/useTitleColorContext"
+
 
 const Home = () => {
 
     // const {counter} = useContext(CounterContext)
     const {counter} = useCounterContext()
 
+    // 5 - contexto mais complexo
+    const {color} = useTitleColorContext()
+
+    console.log(color)
+
   return (
     <div>
-        <h1>Home</h1>
+        <h1 style={{color: color}}>Home</h1>
         <p>Valor do contador: {counter}</p>
         {/* 3 - alterando valor contexto */}
         <ChangeCounter />
