5907fbb8165eb00b7f5b58e0d07ee4905e7144be
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-10-06 23:49:45 -0300

    Create the component Products

--- 9_CONTEXT_API/contextapi/src/App.jsx
+++ 9_CONTEXT_API/contextapi/src/App.jsx
@@ -1,35 +1,31 @@
-import './App.css'
+import "./App.css";
 
-// 1 - config react router
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-// components
-import NavBar from './components/NavBar'
-
+// 1 - config react router, sem links
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // pages
-import Home from './pages/Home'
-import About from './pages/About'
-import Other  from './pages/Other'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
 
+// 2 - adicionando links
+// components
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
-      <div>
-       <h1>Context API</h1>
-       <BrowserRouter>
-        <NavBar />
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/other' element={<Other />} />
-          </Routes>
-
-
-       </BrowserRouter>
-      </div>
-  )
+    <div className="App">
+      <h1>Context</h1>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="products" element={<Products />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
