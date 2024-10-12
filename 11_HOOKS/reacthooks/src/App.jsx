245e039fe3317d8c7f37e8e6a664bd5b727c2a52
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-10-12 18:15:02 -0300

    Create the project reacthooks

---
+++ 11_HOOKS/reacthooks/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+import { BrowserRouter, Router, Routes, Link } from 'react-router-dom'
+
+// pages
+
+function App() {
+  
+  return (
+    
+      <div className='App'>
+        
+      </div>
+  )
+}
+
+export default App
