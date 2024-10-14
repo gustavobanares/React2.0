d9965f838cc131ea21491201d489805468280fd1
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-10-13 21:42:04 -0300

    Reviewing useState

--- 11_HOOKS/reacthooks/src/components/HookUseState.jsx
+++ 11_HOOKS/reacthooks/src/components/HookUseState.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import {useState} from 'react'
 
 const HookUseState = () => {
+    // 1 - useState
+    let userName = 'Bruno'
+
+    const [name, setName] = useState('Gustavo')
+
+    const changeNames = () => {
+        userName = 'Crisitano Ronaldo'
+
+        setName('Gustavo Banares')
+    }
+
   return (
-    <div>HookUseState</div>
-  )
+
+  <div>
+    {/* 1- useState */}
+    <h2>useState</h2>
+    <p>Variavel: {userName}</p>
+    <p>useState: {name}</p>
+    <button onClick={changeNames}></button>
+    <hr />
+  </div>
+)
 }
 
 export default HookUseState
--- 11_HOOKS/reacthooks/src/pages/Home.jsx
+++ 11_HOOKS/reacthooks/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import HookUseState from '../components/HookUseState'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <HookUseState />
+    </div>
   )
 }
 
