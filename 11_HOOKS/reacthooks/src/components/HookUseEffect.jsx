9c71139f94bbb15c982faaad2b97d53b9f5a6f26
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-10-15 00:00:37 -0300

    Empty dependency array in useEffect

--- 11_HOOKS/reacthooks/src/components/HookUseEffect.jsx
+++ 11_HOOKS/reacthooks/src/components/HookUseEffect.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
 
 const HookUseEffect = () => {
+    // 1 - useEffect, sem dependencias
+    useEffect(() =>{
+        console.log('Estou sendo executado')
+    })
+
+    const [number, setNumber] = useState(1)
+
+    const changeSomething = () =>{
+        setNumber(number + 1)
+    }
+    
+    // 2 - array de deps. vazio
+    useEffect(() =>{
+
+        console.log('Serei executado apenas uma vez')
+
+    }, [])
+
+
   return (
     <div>
         <h2>useEffect</h2>
+        <p>Number: {number}</p>
+        <button onClick={changeSomething}>Executar!</button>
         <hr />
     </div>
   )
