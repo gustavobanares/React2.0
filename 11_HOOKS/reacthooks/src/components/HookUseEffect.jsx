242710d5ba9a33d3e6b97772f3f9a39fe9cf08d6
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-10-15 00:06:43 -0300

    Using the dependency array

--- 11_HOOKS/reacthooks/src/components/HookUseEffect.jsx
+++ 11_HOOKS/reacthooks/src/components/HookUseEffect.jsx
@@ -19,12 +19,25 @@ const HookUseEffect = () => {
 
     }, [])
 
+    // 3 - item no array de deps.
+    const [anotherNumber, setAnotherNumber] = useState(0)
+
+    useEffect(() =>{
+
+        if(anotherNumber > 0){
+            console.log('Sou executado apenas quando o anotherNumber muda!')
+        }
+
+    }, [anotherNumber])
+
 
   return (
     <div>
         <h2>useEffect</h2>
         <p>Number: {number}</p>
         <button onClick={changeSomething}>Executar!</button>
+        <p>Another number: {anotherNumber}</p>
+        <button onClick={() => setAnotherNumber(anotherNumber + 1)}>Mudar anotherNumber!</button>
         <hr />
     </div>
   )
